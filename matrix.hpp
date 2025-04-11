0f112d7b6f0fc75febb3edcc9470a80181938ed4
G3N34D18 <g923d101@ku.edu>
2025-04-10 21:24:49 -0500

    fixed another silly error

--- matrix.hpp
+++ matrix.hpp
@@ -47,6 +47,7 @@ public:
         matrix[i][j] = n;
     }
     DataType get_value(std::size_t i, std::size_t j) const {
+        std::size_t N = get_size();
         if (i > N || j > N) {
             throw std::out_of_range("Index out of range.");
         }
